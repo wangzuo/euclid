ab277f91afadbf6ea4a8f776385b7dca058cbf10
Wang Zuo <wzuoadjusted@gmail.com>
2017-11-08 16:16:18 +0800

    npm run build

--- examples/app.js
+++ examples/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Scene from './euler-line.example';
-// import Scene from './simson-line.example';
-import Scene from './s5.example';
+import Scene from './simson-line.example';
+// import Scene from './s5.example';
 
 ReactDOM.render(<Scene />, document.getElementById('app'));
