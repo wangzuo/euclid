bad79e4d5694d05e6b4583bd1a070962f1fd09eb
Wang Zuo <wzuoadjusted@gmail.com>
2018-07-18 17:45:39 +0800

    add index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Link } from 'next';
+
+export default () => (
+  <div>
+    <h1>Euclid</h1>
+    <div>
+      <Link href="/problems">View all problems</Link>
+    </div>
+  </div>
+);
---
+++ pages/problems/index.js
@@ -0,0 +1,11 @@
+import { Link } from 'next';
+
+export default () => (
+  <div>
+    <h2>Problems</h2>
+    <div>
+      <Link href="/problems/euler-line">Euler Line</Link>
+      <Link href="/problems/simson-line">Simson Line</Link>
+    </div>
+  </div>
+);
